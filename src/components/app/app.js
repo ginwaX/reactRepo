65712ac51c681c99082219ce121df33ca09eb07a
pepega <pepega@pepegas-MBP.dlink>
2025-11-19 13:44:35 +0300

    added more state code

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,30 +3,53 @@ import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
+import { Component } from 'react';
 
 import './app.css';
 
-function App() {
+class App extends Component {
 
-    const data = [
-        {name: 'John C.', salary: 800, increase: false},
-        {name: 'Alex M.', salary: 3000, increase: true},
-        {name: 'Carl W.', salary: 5000, increase: false}
-    ];
+    constructor(props) {
+        super(props);
+        this.state = {
+             data : [
+            {name: 'John C.', salary: 800, increase: false, id: 1},
+            {name: 'Alex M.', salary: 3000, increase: true, id: 2},
+            {name: 'Carl W.', salary: 5000, increase: false, id: 3}
+            ],
+        };
+    }
 
-    return (
-        <div className="app">
-            <AppInfo />
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            //const index = data.findIndex(elem => elem.id === id);
+            // const before = data.slice(0, index);
+            // const after = data.slice(index + 1);
+            // const newArr = [...before, ...after];
+            return {
+                data: data.filter(item => item.id != id),
+            }
+        });
+    }
 
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+    render()
+    {
+        return (
+            <div className="app">
+                <AppInfo />
+
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+                
+                <EmployeesList 
+                    data={this.state.data}
+                    onDelete={this.deleteItem}/>
+                <EmployeesAddForm/>
             </div>
-            
-            <EmployeesList data={data}/>
-            <EmployeesAddForm/>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,30 +1,50 @@
 import './employees-list-item.css';
+import { Component } from 'react';
 
-const EmployeesListItem = ({name, salary, increase}) => {
+class EmployeesListItem extends Component {
 
-    let classNames = "list-group-item d-flex justify-content-between";
-    if (increase) {
-        classNames += ' increase';
+    constructor(props) {
+        super(props);
+        this.state = {
+            increase: false,
+        };
+    }
+    // change if the employee gets extra salary while clicking on the cookie button
+    onIncrease = () => {
+        this.setState(({increase}) => ({
+            increase: !increase
+        }));
     }
 
-    return (
-        <li className={classNames}>
-            <span className="list-group-item-label">{name}</span>
-            <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
-            <div className='d-flex justify-content-center align-items-center'>
-                <button type="button"
-                    className="btn-cookie btn-sm ">
-                    <i className="fas fa-cookie"></i>
-                </button>
+    render() {
+        const {name, salary, onDelete} = this.props;
+        const {increase} = this.state;
+        let classNames = "list-group-item d-flex justify-content-between";
+        if (increase) {
+            classNames += ' increase';
+        }
 
-                <button type="button"
-                        className="btn-trash btn-sm ">
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    )
+        return (
+            <li className={classNames}>
+                <span className="list-group-item-label">{name}</span>
+                <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
+                <div className='d-flex justify-content-center align-items-center'>
+                    <button type="button"
+                        className="btn-cookie btn-sm "
+                        onClick={this.onIncrease}>
+                        <i className="fas fa-cookie"></i>
+                    </button>
+
+                    <button type="button"
+                            className="btn-trash btn-sm "
+                            onClick={onDelete}>
+                        <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        )
+    }
 }
 
 export default EmployeesListItem;
